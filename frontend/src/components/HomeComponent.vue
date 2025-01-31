806ae1f95565658519b804d0f879fa03e9384da6
Chirac Michoagan <michoaganchiracdona@gmail.com>
2025-01-31 12:04:53 +0100

    instalation du project front

---
+++ frontend/src/components/HomeComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div class="text-3xl font-bold text-teal-400 underline cursor-pointer">Hello Tailwind CSS!</div>
+    <UserPen :size="32" class="inline-block ml-2" />
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+import { UserPen } from 'lucide-vue-next';
+</script>
+<style scoped></style>
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import ToolBar from '@/components/ToolBar.vue';
+import CanvasComponent from '@/components/CanvasComponent.vue';
+import HomeComponent from '@/components/HomeComponent.vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,16 +9,19 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: HomeComponent
+    },
+
+    {
+      path: '/cancas',
+      name: 'canvas',
+      component: CanvasComponent
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
+      path: '/toolbar',
+      name: 'toolbar',
+      component: ToolBar
+    }
   ],
 })
 
